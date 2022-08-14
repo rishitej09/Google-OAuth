8716783ffe4cf10dace31367d4ae2041f277f3d1
rishitej09 <rishitej0909@gmail.com>
2022-08-14 12:19:41 +0530

    Complete Authentication

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express = require ("express");
 const ejs = require ("ejs");
 const bodyParser = require ("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require ("md5");
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+const passport = require("passport");
 
 const app = express();
 app.use(express.static("public"));
@@ -19,15 +24,41 @@ const userSchema = new mongoose.Schema({
 
 
 
-const secret = process.env.SECRET ;
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+// const secret = process.env.SECRET ;
+// userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
+
+userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User",userSchema);
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "https://localhost:3000/auth/google/OAuth",
+    userProfileURL:"https://www.googleapis.com/oauth3/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.get( "/" , function ( req , res ){
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate("google",{scope:['profile']})
+    );
+
+app.get('/auth/google/OAuth', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect("/secrets");
+  });
 
 app.get( "/login" , function ( req , res ){
     res.render("login");
@@ -40,7 +71,7 @@ app.get( "/register" , function ( req , res ){
 app.post( "/register" , function ( req , res ){
     const newUser = new User ({
         email: req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -54,7 +85,7 @@ app.post( "/register" , function ( req , res ){
 
 app.post ("/login" , function ( req , res ){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},function ( err , foundUser){
         if(err){
